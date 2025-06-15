3639fa16431cfe6e0d57596b0c243231e947911e
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-15 22:02:26 +0900

    add: 使用ライブラリの追加

---
+++ src/types/css.d.ts
@@ -0,0 +1,9 @@
+// src/types/css.d.ts
+declare module '*.css';
+
+declare module "swiper/css";
+declare module "swiper/css/navigation";
+declare module "swiper/css/pagination";
+declare module "swiper/css/scrollbar";
+declare module '@splidejs/react-splide';
+declare module '@splidejs/react-splide/css';
